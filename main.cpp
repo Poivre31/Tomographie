41e682f86514ca7a732aafd90e63acbf4fec381d
Poivre31 <poivre.astro@gmail.com>
2026-05-09 12:08:49 +0200

    Amélioration de la fonctionalité d'affichage d'image: possibilité d'afficher plusiseurs images sur un même plot et d'assigner un niveau gamma pour chacune d'entre elles.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main()
     // exemple_fft_1D(1024, 30);
     // exemple_fft_2D(1024, 16);
 
-    size_t size = 1024;
+    size_t size = 256;
     image phantom(size, size);
 
     // phantom.fill_ellipse(1., size / 2, size / 2, size / 16, size / 16);
-    // phantom.fill_rectangle(1, size / 2, size / 2, size / 4, size / 4);
+    // phantom.fill_rectangle(1, size / 2, size / 2, size / 16, size / 16);
     phantom.fill_phantom(size / 2, size / 2, .8 * size);
     auto phantom_fft = fft_2D_shift(fft_2D(phantom));
 
@@ -38,8 +38,11 @@ int main()
             double y = size / 2. + sin(theta) * (i - size / 2.);
             double phase = std::arg(phantom_fft.get(ceil(x), ceil(y)));
             complex z = abs(v[i]) * exp(1i * phase);
-            polar.increment(ceil(x), ceil(y), z);
-            weight.increment(ceil(x), ceil(y), 1);
+            if (abs(z) > 0)
+            {
+                polar.increment(ceil(x), ceil(y), v[i]);
+                weight.increment(ceil(x), ceil(y), 1);
+            }
             // if (abs(i - size / 2) > size / 8)
             //     polar.set(ceil(x), ceil(y), 0);
         }
@@ -56,11 +59,15 @@ int main()
 
     polar.rotate(1, 1);
 
-    display_image(phantom_fft.modulus_to_image());
-    display_image(polar.modulus_to_image());
-    display_image(phantom);
-    display_image(ffti_2D(polar).modulus_to_image());
-    // display_image(projection);
+    display_images({phantom_fft.phase_to_image(), polar.phase_to_image(), projection}, 3);
+    display_images({phantom_fft.phase_to_image(), polar.phase_to_image(), phantom, projection}, 1);
+    display_images({phantom_fft.modulus_to_image(), polar.modulus_to_image(), phantom, projection}, {4., 2., 1., 1.});
+
+    // display_image(phantom_fft.phase_to_image(), 1);
+    // display_image(polar.phase_to_image(), 1);
+    // display_image(phantom);
+    // // display_image(projection);
+    // display_image(ffti_2D(polar).modulus_to_image());
 
     if (false)
     {
--- python/display_image.py
+++ python/display_image.py
@@ -2,16 +2,37 @@ import numpy as np
 from matplotlib import pyplot as plt
 import sys
 
-img = open(sys.argv[1])
-width = int(img.readline())
-height = int(img.readline())
+n_image = (len(sys.argv) - 1) // 2
 
-image = np.empty((height, width))
+n_plot_h = n_image
+n_plot_v = 1
 
-for i in range(height):
-    for j in range(width):
-        image[i, j] = float(img.readline())
+if n_image > 2:
+    n_plot_h = 2
+    n_plot_v = 2
+if n_image > 4:
+    n_plot_h = 3
+if n_image > 6:
+    n_plot_h = 4
+if n_image > 8:
+    n_plot_v = 3
 
+for k in range(1, n_image + 1):
+    img = open(sys.argv[k])
+    width = int(img.readline())
+    height = int(img.readline())
 
-plt.imshow(np.sqrt(image), cmap="gray")
+    image = np.empty((height, width))
+
+    for i in range(height):
+        for j in range(width):
+            image[i, j] = float(img.readline())
+
+    plt.subplot(n_plot_v, n_plot_h, k)
+    gamma = float(sys.argv[k + n_image])
+    if gamma == 1:
+        plt.imshow(image, cmap="gray")
+    else:
+        plt.imshow(abs(image) ** (1 / gamma), cmap="gray")
+
 plt.show()
